3ee540c014de786c2c9474fbe4ec61cb23357c52
Aaron Kendall <aaron.kendall@arcadiagroup.co.uk>
2018-01-25 17:20:54 +0000

    turns out we're safe against the web3 deprecation message thing as we use truffle contract like legends and just pass the provider in, also using promise based method to get logged in eth account so hopefully the page refreshness is less flakey

--- src/app/utils/web3Setup.js
+++ src/app/utils/web3Setup.js
@@ -3,12 +3,15 @@ import { initApp } from '../actions/coreActions';
 
 export default function initialiseWeb3(dispatch) {
   if (typeof window.web3 !== 'undefined') {
-    const ethProvider = web3.currentProvider;
-
-    if (web3.eth.defaultAccount) {
-      // This needs to be added to state here as it doesn't persist otherwise and
-      // we get some weird errors occasionally with the address being undefined
-      dispatch(initApp(web3.eth.defaultAccount, ethProvider))
-    }
+    const ethProvider = web3.currentProvider
+    
+    web3.eth.getAccounts()
+      .then(accounts => {
+        if (accounts.length > 0) {
+          // This needs to be added to state here as it doesn't persist otherwise and
+          // we get some weird errors occasionally with the address being undefined
+          dispatch(initApp(accounts[0], ethProvider))
+        }
+      })
   }
 }
