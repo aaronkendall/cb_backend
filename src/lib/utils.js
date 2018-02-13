d0c15ffaa6dc63de49a4354d50716ecf0862f8c8
Aaron Kendall <aaron.kendall@arcadiagroup.co.uk>
2018-02-13 12:43:45 +0000

    filter string to lower case before we do anything to standardise it

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,12 +5,13 @@ const calculateLevel = (fighter) => {
 const filterStringToQueryObject = (filterString) => {
   // 'speed: 1, level: 3'
   return filterString
+    .toLowerCase()
     .replace(/\s/g, '')
     .split(',')
     .reduce((queryObject, queryString) => {
       const queryArray = queryString.split(':')
       // Need to make sure that if we query by max level that it is in camelcase
-      if (queryArray[0].toLowerCase().includes('max')) queryArray[0] = 'maxLevel'
+      if (queryArray[0].includes('max')) queryArray[0] = 'maxLevel'
 
       queryObject[`fighter.${queryArray[0]}`] = { $gte: queryArray[1] }
       return queryObject
