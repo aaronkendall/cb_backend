058fc9dc1dfe6a2d48fc536e2ea0c9e97ad64f4a
Aaron <aarongkendall@gmail.com>
2018-04-08 12:34:07 +0100

    querying by price potentially fixed, not totally sure

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,5 @@
+const ethunits = require('ethereum-units')
+
 const calculateLevel = (fighter) => {
   return Math.floor((fighter.maxHealth.toNumber() + fighter.speed.toNumber() + fighter.strength.toNumber()) / 10);
 }
@@ -11,7 +13,8 @@ const filterStringToQueryObject = (filterString, initialQuery = {}) => {
     .reduce((queryObject, queryString) => {
       const queryArray = queryString.split(':')
       // Need to make sure that if we query by max level that it is in camelcase
-      if (queryArray[0].includes('max')) queryArray[0] = 'maxLevel'
+      // if (queryArray[0].includes('max')) queryArray[0] = 'maxLevel'
+      if (queryArray[0] === 'price') queryArray[1] = ethunits.convert(parseFloat(queryArray[1]), 'ether', 'wei')
 
       queryObject[queryArray[0]] = { $gte: queryArray[1] }
       return queryObject
